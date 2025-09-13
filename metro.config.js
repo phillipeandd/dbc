9cdef4fa9e6992483d029301071f2269610649c3
Phillip Port <ed1907006@gmail.com>
2025-09-13 17:22:50 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,17 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add support for additional file extensions
-config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj', 'png', 'jpg');
-
-// Add support for resolving modules
-config.resolver.alias = {
-  '@': __dirname,
-  '@components': `${__dirname}/components`,
-  '@hooks': `${__dirname}/hooks`,
-  '@utils': `${__dirname}/utils`,
-  '@theme': `${__dirname}/theme`,
-  '@styles': `${__dirname}/styles`,
-};
-
 module.exports = config;
