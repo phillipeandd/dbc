98c1d30999518583658e9b221f320b6292753509
Phillip Port <ed1907006@gmail.com>
2025-09-13 18:34:53 +0530

    Updated App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/context/AuthProvider';
 import RootScreen from './src/components/RootScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <RootScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <View style={styles.container}>
+          <RootScreen />
+          <StatusBar style="auto" />
+        </View>
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
